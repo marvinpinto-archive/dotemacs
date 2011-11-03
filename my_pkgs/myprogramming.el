81aa2c400f7dac821da99235b95ba9aece58ce18
Marvin Pinto <me@marvinp.ca>
2011-11-03 14:37:54 -0400

    Minor updates for YAML mode.

--- my_pkgs/myprogramming.el
+++ my_pkgs/myprogramming.el
@@ -175,6 +175,7 @@
 ;; YAML Mode
 (autoload 'yaml-mode "yaml-mode.el" "Major mode for editing YAML files")
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 
 
 ;; Apache Mode
